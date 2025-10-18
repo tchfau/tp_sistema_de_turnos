942d5903ef496a2ce9e1b01d82cbd3fa42db20dc
Ignacio Martin <tchfau@gmail.com>
2025-10-18 18:57:01 -0300

    Update principal.py
    Se corrige código inicializando las variables dentro del constructor ya que estaban como globales

--- principal.py
+++ principal.py
@@ -3,114 +3,115 @@ from transformador import Transformador
 
 #------------ Programa principal --------------- 
 
-
-archivo_clientes = open("clientes.csv", "rt")
-archivo_empleados = open("empleados.csv", "rt")
-llaves_clientes = archivo_clientes.readline()
-llaves_empleados = archivo_empleados.readline()
-cliente = Transformador(llaves_clientes)
-empleado = Transformador(llaves_empleados)
-
-lista_clientes = []
-lista_empleados = []
-
-line_cli = archivo_clientes.readline()
-while line_cli != "":
-    if line_cli == "\n":  # saltar si la líena está vacía
-        line_cli = archivo_clientes.readline()
-        continue
-
-    d = cliente.str2dict(line_cli)
-    lista_clientes.append(d)
-    line_cli = archivo_clientes.readline() 
-
-
-line_emp = archivo_empleados.readline()
-while line_emp != "":
-    if line_emp == "\n":  # saltar si la líena está vacía
-        line_emp = archivo_empleados.readline()
-        continue
-
-    d = empleado.str2dict(line_emp)
-    lista_empleados.append(d)
-    line_emp = archivo_empleados.readline() 
-
-archivo_clientes.close()
-archivo_empleados.close()
-
 class GestorTurnos(object):
 
-    def registrar_cliente():
-        nuevo = cliente.ingresar_valores()
-        lista_clientes.append(nuevo)
+    def __init__(self):
+        # Carga y lectura de l archivo clientes
+        archivo_clientes = open("clientes.csv", "rt")
+        llaves_clientes = archivo_clientes.readline()
+        self.cliente = Transformador(llaves_clientes)
+        self.lista_clientes = []
+
+        line_cli = archivo_clientes.readline()
+        while line_cli != "":
+            if line_cli == "\n":  # saltar si la línea está vacía
+                line_cli = archivo_clientes.readline()
+                continue
+            d = self.cliente.str2dict(line_cli)
+            self.lista_clientes.append(d)
+            line_cli = archivo_clientes.readline()
+        archivo_clientes.close()
+
+        # Carga y lectura del archivo empleados
+        archivo_empleados = open("empleados.csv", "rt")
+        llaves_empleados = archivo_empleados.readline()
+        self.empleado = Transformador(llaves_empleados)
+        self.lista_empleados = []
+
+        line_emp = archivo_empleados.readline()
+        while line_emp != "":
+            if line_emp == "\n":  # saltar si la línea está vacía
+                line_emp = archivo_empleados.readline()
+                continue
+            d = self.empleado.str2dict(line_emp)
+            self.lista_empleados.append(d)
+            line_emp = archivo_empleados.readline()
+        archivo_empleados.close()
+
+    # Métodos del gestor 
+    def registrar_cliente(self):
+        nuevo = self.cliente.ingresar_valores()
+        self.lista_clientes.append(nuevo)
         print("Cliente registrado correctamente")
 
-    def registrar_nuevo_empleado():
-        nuevo = empleado.ingresar_valores()
-        lista_empleados.append(nuevo)
-        print("Empleado registrado correctaemente")
+    def registrar_nuevo_empleado(self):
+        nuevo = self.empleado.ingresar_valores()
+        self.lista_empleados.append(nuevo)
+        print("Empleado registrado correctamente")
 
-    def solicitar_turno():
+    def solicitar_turno(self):
         print("Solicitando un nuevo turno")
 
-    def listar_trunos():
+    def listar_turnos(self):
         print("Se listan los turnos")
 
-    def modificar_turno():
+    def modificar_turno(self):
         print("Se modifica turno existente")
 
-    def cancelar_turno():
+    def cancelar_turno(self):
         print("Se cancela turno")
 
-    def guardar_datos():
+    def guardar_datos(self):
         print("se guardan datos en archivo CSV")
 
+    # Menú interactivo
+    def mostrar_menu(self):
+        print("**************************************")
+        print("**************************************")
+        print('SISTEMA DE GESTIÓN DE TURNOS')
+        print("**************************************")
+        print("--------------------------------------")
+        print("MENU PRINCIPAL")
+        print("--------------------------------------")
 
+        menu = """
+[1] Registrar nuevo Cliente
+[2] Registrar nuevo Empleado
+[3] Solicitar turno
+[4] Listar turnos existentes
+[5] Modificar turno
+[6] Cancelar turno
+[7] Guardar datos 
+[8] Salir
+"""
+        print(menu)
 
-    print("**************************************")
-    print("**************************************")
-    print('SISTEMA DE GESTIÓN DE TURNOS')
-    print("**************************************")
-    print("--------------------------------------")
-    print("MENU PRINCIPAL")
-    print("--------------------------------------")
-
-    menu = """
-    [1] Registrar nuevo Cliente
-    [2] Registrar nuevo Empleado
-    [3] Solicitar turno
-    [4] Listar turnos existentes
-    [5] Modificar turno
-    [6] Cancelar turno
-    [7] Guardar datos 
-    [8] Salir
-
-    """
-    print(menu)
-
-    def mostrar_menu():
         while True:
             opcion = input("Seleccione la opción deseada (1 - 8): ")
             if opcion == "1":
-                GestorTurnos.registrar_cliente()
+                self.registrar_cliente()
             elif opcion == "2":
-                GestorTurnos.registrar_nuevo_empleado()
+                self.registrar_nuevo_empleado()
             elif opcion == "3":
-                GestorTurnos.solicitar_turno()
+                self.solicitar_turno()
             elif opcion == "4":
-                GestorTurnos.listar_trunos()
+                self.listar_turnos()
             elif opcion == "5":
-                GestorTurnos.modificar_turno()
+                self.modificar_turno()
             elif opcion == "6":
-                GestorTurnos.cancelar_turno()
+                self.cancelar_turno()
             elif opcion == "7":
-                GestorTurnos.guardar_datos()
+                self.guardar_datos()
             elif opcion == "8":
-                print("Saliendo del programa")
+                print("Saliendo del programa.")
                 break
             else:
-                print("Opción inválida. Intente nuevamente")
+                print("Opción inválida. Intente nuevamente.")
 
-GestorTurnos.mostrar_menu()
-print("Lista de empleados:\n", lista_empleados)
-print("Lista de clientes:\n", lista_clientes)
+        print("Lista de empleados:\n", self.lista_empleados)
+        print("Lista de clientes:\n", self.lista_clientes)
+
+
+# Ejecución del programa
+gestor = GestorTurnos()
+gestor.mostrar_menu()
