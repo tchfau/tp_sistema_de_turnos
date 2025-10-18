a0124927e153458499b2bac722df652e4441d46f
Ignacio Martin <tchfau@gmail.com>
2025-10-18 17:15:17 -0300

    Update principal.py
    Crea la clase GestorTurno

--- principal.py
+++ principal.py
@@ -3,6 +3,7 @@ from transformador import Transformador
 
 #------------ Programa principal --------------- 
 
+
 archivo_clientes = open("clientes.csv", "rt")
 archivo_empleados = open("empleados.csv", "rt")
 llaves_clientes = archivo_clientes.readline()
@@ -37,79 +38,79 @@ while line_emp != "":
 archivo_clientes.close()
 archivo_empleados.close()
 
+class GestorTurno(object):
+
+    def registrar_cliente():
+        nuevo = cliente.ingresar_valores()
+        lista_clientes.append(nuevo)
+        print("Cliente registrado correctamente")
+
+    def registrar_nuevo_empleado():
+        nuevo = empleado.ingresar_valores()
+        lista_empleados.append(nuevo)
+        print("Empleado registrado correctaemente")
+
+    def solicitar_turno():
+        print("Solicitando un nuevo turno")
+
+    def listar_trunos():
+        print("Se listan los turnos")
+
+    def modificar_turno():
+        print("Se modifica turno existente")
+
+    def cancelar_turno():
+        print("Se cancela turno")
+
+    def guardar_datos():
+        print("se guardan datos en archivo CSV")
 
 
-def registrar_cliente():
-    nuevo = cliente.ingresar_valores()
-    lista_clientes.append(nuevo)
-    print("Cliente registrado correctamente")
 
-def registrar_nuevo_empleado():
-    nuevo = empleado.ingresar_valores()
-    lista_empleados.append(nuevo)
-    print("Empleado registrado correctaemente")
+    print("**************************************")
+    print("**************************************")
+    print('SISTEMA DE GESTIÓN DE TURNOS')
+    print("**************************************")
+    print("--------------------------------------")
+    print("MENU PRINCIPAL")
+    print("--------------------------------------")
 
-def solicitar_turno():
-    print("Solicitando un nuevo turno")
+    menu = """
+    [1] Registrar nuevo Cliente
+    [2] Registrar nuevo Empleado
+    [3] Solicitar turno
+    [4] Listar turnos existentes
+    [5] Modificar turno
+    [6] Cancelar turno
+    [7] Guardar datos 
+    [8] Salir
 
-def listar_trunos():
-    print("Se listan los turnos")
+    """
+    print(menu)
 
-def modificar_turno():
-    print("Se modifica turno existente")
+    def mostrar_menu():
+        while True:
+            opcion = input("Seleccione la opción deseada (1 - 8): ")
+            if opcion == "1":
+                GestorTurno.registrar_cliente()
+            elif opcion == "2":
+                GestorTurno.registrar_nuevo_empleado()
+            elif opcion == "3":
+                GestorTurno.solicitar_turno()
+            elif opcion == "4":
+                GestorTurno.listar_trunos()
+            elif opcion == "5":
+                GestorTurno.modificar_turno()
+            elif opcion == "6":
+                GestorTurno.cancelar_turno()
+            elif opcion == "7":
+                GestorTurno.guardar_datos()
+            elif opcion == "8":
+                print("Saliendo del programa")
+                break
+            else:
+                print("Opción inválida. Intente nuevamente")
 
-def cancelar_turno():
-    print("Se cancela turno")
-
-def guardar_datos():
-    print("se guardan datos en archivo CSV")
-
-
-
-print("**************************************")
-print("**************************************")
-print('SISTEMA DE GESTIÓN DE TURNOS')
-print("**************************************")
-print("--------------------------------------")
-print("MENU PRINCIPAL")
-print("--------------------------------------")
-
-menu = """
-[1] Registrar nuevo Cliente
-[2] Registrar nuevo Empleado
-[3] Solicitar turno
-[4] Listar turnos existentes
-[5] Modificar turno
-[6] Cancelar turno
-[7] Guardar datos 
-[8] Salir
-
-"""
-print(menu)
-
-def mostrar_menu():
-    while True:
-        opcion = input("Seleccione la opción deseada (1 - 8): ")
-        if opcion == "1":
-            registrar_cliente()
-        elif opcion == "2":
-            registrar_nuevo_empleado()
-        elif opcion == "3":
-            solicitar_turno()
-        elif opcion == "4":
-            listar_trunos()
-        elif opcion == "5":
-            modificar_turno()
-        elif opcion == "6":
-            cancelar_turno()
-        elif opcion == "7":
-            guardar_datos()
-        elif opcion == "8":
-            print("Saliendo del programa")
-            break
-        else:
-            print("Opción inválida. Intente nuevamente")
-
-mostrar_menu()
+GestorTurno.mostrar_menu()
 print("Lista de empleados:\n", lista_empleados)
 print("Lista de clientes:\n", lista_clientes)
