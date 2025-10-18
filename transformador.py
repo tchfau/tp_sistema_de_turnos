480431ee902ffc9c2d1ae078146abe0cbea214ca
Ignacio Martin <tchfau@gmail.com>
2025-10-18 15:30:43 -0300

    Update transformador.py
    Se implementa el método ingresar_valores para que sean convertidos y asociados a las llaves correspondientes invocándolo desde el menú interactivo en el programa principal

--- transformador.py
+++ transformador.py
@@ -11,4 +11,16 @@ class Transformador(object):
             d[self.keys[j]] = fila[j]
             j = j + 1
         return d
+    
+    def ingresar_valores(self):
+        #Permite ingresar valores desde teclado y genera un diccionario nuevo
+        print("Ingrese los valores para las siguientes claves:")
+        fila = []
+        j = 0
+        while j < len(self.keys):
+            valor = input(f"{self.keys[j]}: ")
+            fila.append(valor)
+            j = j + 1
+        valores = ",".join(fila)
+        return self.str2dict(valores)
 
