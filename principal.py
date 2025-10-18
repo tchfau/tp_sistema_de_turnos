f46cf7584ca5063cb86b1b0296bd46b500b48a5f
Ignacio Martin <tchfau@gmail.com>
2025-10-18 11:43:49 -0300

    Agrega menu interactivo con opciones

---
+++ principal.py
@@ -0,0 +1,22 @@
+#------------ Programa principal --------------- 
+
+print("**************************************")
+print("**************************************")
+print('SISTEMA DE GESTIÓN DE TURNOS')
+print("**************************************")
+print("--------------------------------------")
+print("Seleccione la opción deseada:")
+print("--------------------------------------")
+
+menu = """
+[1] Registrar nuevo Cliente
+[2] Registrar nuevo Empleado
+[3] Solicitar turno
+[4] Listar turnos existentes
+[5] Modificar turno
+[6] Cancelar turno
+[7] Guardar datos 
+[8] Salir
+
+"""
+print(menu)
